78f33146956c43f9a0600586d8c829569fdcf3cc
aoifemcdonagh <aoifemcdonagh95@hotmail.com>
2020-08-04 17:45:23 +0100

    setup script to be run in docker container for preprocessing.py

---
+++ setup_preprocessing.sh
@@ -0,0 +1,10 @@
+# build docker image using Dockerfile in InstanceSegmentation_Sentinel2 repo (branch jupyter_cmd)
+# run docker image based on ubuntu18.04 (named fe/test1 in this case..)
+# docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --runtime=nvidia -v /home/researcher02/field_project:/field_project --rm -it fe/test1
+apt update
+apt -y install python3-pip
+apt install nano git python3-tk
+
+pip3 install shapely==1.6.4
+pip3 install matplotlib geopandas rasterio tqdm descartes scikit-image pathlib pprint
+
